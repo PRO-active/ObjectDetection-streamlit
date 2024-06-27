aa1e1ee13bdd709b933b640aba19e80c8695de15
PRO-active <hayamatsu8810@gmail.com>
2024-06-27 23:21:00 +0900

    [WIP]

--- load_model.py
+++ load_model.py
@@ -1,7 +1,7 @@
 import torch
 
 # YOLOv5のモデルをロード
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 # モデルを保存
 torch.save(model, 'yolov5s.pt')
